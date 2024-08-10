42435d778dab3d6611bcd39845a43b5efc8bb9b6
shyamsundarpsgit <shyamsms466340@gmail.com>
2024-08-10 10:51:50 +0530

    Initial

---
+++ app.js
@@ -0,0 +1,28 @@
+
+const port = 3000;
+const path = require('path');
+const express = require('express');
+const http = require('http');
+const app = express();
+const server = http.createServer(app);
+const socketIo = require('socket.io');
+
+const io = socketIo(server);
+//Run when client connects
+io.on('connection',(socket)=>{
+    console.log('New WS connection')
+})
+
+//Set static folder
+app.use(express.static(path.join(__dirname,'public')));
+
+
+
+server.listen(port,(err)=>{
+    if(err){
+        console.log(err.message);
+    }
+    else{
+        console.log("Server created successfully");
+    }
+})
