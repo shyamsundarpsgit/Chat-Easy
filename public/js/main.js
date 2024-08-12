ac665ef0ebcfa96c2d5dd3b12388b5d03dda2260
shyamsundarpsgit <shyamsms466340@gmail.com>
2024-08-12 10:04:15 +0530

    client side updated

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,46 @@
+const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
+
+//Get Username and Room from URL
+const {username,room} = Qs.parse(location.search,{
+    ignoreQueryPrefix:true
+});
+
 const socket = io();
+
+//Join chatroom
+socket.emit('joinRoom',{username,room});
+
+//Message from server
+socket.on('message',(message)=>{
+    console.log(message);
+   outputMessage(message);
+   chatMessages.scrollTop = chatMessages.scrollHeight;
+})
+
+
+//Message submit
+chatForm.addEventListener('submit',(e)=>{
+    e.preventDefault();
+
+    //Get Message Text
+    const msg = e.target.elements.msg.value;
+ 
+    //Emit message to server
+    socket.emit('chatMessage',msg);
+
+    //Clear  input
+    e.target.elements.msg.value = ' ';
+    e.target.elements.msg.focus();
+});
+
+//Output message to DOM
+function outputMessage(message){
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
+          <p class="text">
+            ${message.text}
+          </p>` ;
+    document.querySelector('.chat-messages').appendChild(div);
+}
